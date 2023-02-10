a1c9a175883c5c8b3514f6c72d80c0af4e77d247
Alan Bahena <alanbahena1@gmail.com>
2023-02-10 12:38:06 -0800

    Doctors section integrated (Desktop & Mobile version)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "./UI/Layout";
 import Content from "./components/Header/Content";
 import Treatments from "./components/Treatments/Treatments";
+import Doctors from "./components/Doctors/Doctors";
 
 function App() {
   return (
     <Layout>
       <Content />
       <Treatments />
+      <Doctors />
     </Layout>
   );
 }
---
+++ src/components/Doctors/Doctors.js
@@ -0,0 +1,75 @@
+import React, { Fragment } from "react";
+
+import Doctor1 from "../../assets/images/Dr.jpg";
+
+import clasess from "./Doctors.module.scss";
+
+const Doctors = () => {
+  return (
+    <Fragment>
+      <div className={clasess.background}></div>
+      <div className={clasess.container}>
+        <div className={clasess["container-content"]}>
+          <img src={Doctor1} alt="Doctor"></img>
+          <div>
+            <h3>DR.MIGUEL MAGAÑA</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation.
+            </p>
+          </div>
+        </div>
+        <div className={clasess["container-content"]}>
+          <div>
+            <h3>DR.MIGUEL MAGAÑA</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation.
+            </p>
+          </div>
+          <img src={Doctor1} alt="Doctor"></img>
+        </div>
+      </div>
+      <div className={clasess["container-2"]}>
+        <div className={clasess["container-content"]}>
+          <img src={Doctor1} alt="Doctor"></img>
+          <div>
+            <h3>DR.MIGUEL MAGAÑA</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation.
+            </p>
+          </div>
+        </div>
+        <div className={clasess["container-content"]}>
+          <div>
+            <h3>DR.MIGUEL MAGAÑA</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation.
+            </p>
+          </div>
+          <img src={Doctor1} alt="Doctor"></img>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Doctors;
